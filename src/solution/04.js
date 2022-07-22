649a9ad050ea20a2e43ffde1444d41fb404c6c2c
Jackline Jebet <jebetjackline2016@gmail.com>
2022-07-22 13:23:36 +0300

    Done

--- src/solution/04.js
+++ src/solution/04.js
@@ -1,13 +1,38 @@
 import { useEffect, useState } from "react";
 
+function getLocalStorageValue(key) {
+  const storedValue = localStorage.getItem(key);
+  try {
+    return JSON.parse(storedValue);
+  } catch {}
+  return storedValue;
+}
+
+function setLocalStorageValue(key, value) {
+  const valueToStore = JSON.stringify(value);
+  localStorage.setItem(key, valueToStore);
+}
+
 export function useLocalStorage(key, initialValue = null) {
-  const [state, setState] = useState(localStorage.getItem(key) || initialValue);
+  const storedValue = getLocalStorageValue(key);
+  const [state, setState] = useState(storedValue || initialValue);
 
   useEffect(() => {
-    if (state !== null) {
-      localStorage.setItem(key, state);
-    }
+    setLocalStorageValue(key, state);
   }, [key, state]);
+
+  useEffect(() => {
+    function handleChange() {
+      const newValue = getLocalStorageValue(key);
+      setState(newValue);
+    }
+
+    window.addEventListener("storage", handleChange);
+
+    return function cleanup() {
+      window.removeEventListener("storage", handleChange);
+    };
+  }, [key]);
 
   return [state, setState];
 }
